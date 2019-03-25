ea0355578b22d462c2cc055138f52ee0722b4ae4
vnctwong <vincentkywong@gmail.com>
2019-03-25 23:34:27 +0000

    not working, commit to switch branch

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require_relative "./mail_settings"
+
+ActionMailer::Base.smtp_settings = getMailSettings
 
 # Initialize the Rails application.
 Rails.application.initialize!
